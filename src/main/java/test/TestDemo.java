8183df665fa51012c227324ca37a799a67da2323
Rita <772482292@qq.com>
2021-09-09 17:12:59 +0800

    second commit

--- src/main/java/test/TestDemo.java
+++ src/main/java/test/TestDemo.java
@@ -3,5 +3,8 @@ package test;
 public class TestDemo {
     public static void main(String[] args) {
         System.out.printf("hello git!");
+        System.out.printf("hello git!");
+        System.out.printf("hello git!");
+        System.out.printf("hello git!");
     }
 }
